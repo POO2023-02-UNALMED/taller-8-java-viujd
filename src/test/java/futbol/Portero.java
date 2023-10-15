b47c1df6b9a11329a9df48019201df4c74c379f4
Juan David Robayo <141962352+viujd@users.noreply.github.com>
2023-10-14 19:41:14 -0500

    Update Portero.java

--- src/test/java/futbol/Portero.java
+++ src/test/java/futbol/Portero.java
@@ -4,7 +4,7 @@ public class Portero extends Futbolista {
 	public short golesRecibidos;
 	public byte dorsal;
 	
-	public Portero (String nombre, int edad, String posicion, short golesRecibidos, byte dorsal) {
+	public Portero (String nombre, int edad, short golesRecibidos, byte dorsal) {
 		super(nombre, edad, "Portero");
 		this.golesRecibidos=golesRecibidos;
 		this.dorsal=dorsal;
